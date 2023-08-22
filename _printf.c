feddf4687a1707d40d96320282ca6bc0cb6b74f3
Nhlapo <maphoskho@gmail.com>
2023-08-22 01:46:16 -0700

    printf fixing errors on the checker

--- _printf.c
+++ _printf.c
@@ -1,66 +1,49 @@
 #include "main.h"
 
-void print_buffer(char buffer[], int *buff_ind);
+int _printf(const char *format, ...) {
+	va_list args;
+	va_start(args, format);
+    
+	int count = 0;
 
-/**
- * _printf - Printf function
- * @format: frmat.
- * Return: Printed chars.
- */
-int _printf(const char *format, ...)
-{
-	int i, printed = 0, printed_chars = 0;
-	int flags, width, precision, size, buff_ind = 0;
-	va_list list;
-	char buffer[BUFF_SIZE];
+	while (*format != '\0') {
+		if (*format == '%') {
+			format++; // Move past '%'
 
-	if (format == NULL)
-		return (-1);
+			if (*format == '\0') {
+				break; // If format ends with '%', stop
+			}
 
-	va_start(list, format);
-
-	for (i = 0; format && format[i] != '\0'; i++)
-	{
-		if (format[i] != '%')
-		{
-			buffer[buff_ind++] = format[i];
-			if (buff_ind == BUFF_SIZE)
-				print_buffer(buffer, &buff_ind);
-			/* write(1, &format[i], 1);*/
-			printed_chars++;
+			if (*format == 'c') {
+				int ch = va_arg(args, int);
+				putchar(ch);
+				count++;
+			} else if (*format == 's') {
+				char *str = va_arg(args, char *);
+					while (*str != '\0') {
+					putchar(*str);
+					str++;
+					count++;
+				}
+			} else if (*format == '%') {
+				putchar('%');
+				count++;
+			} else {
+			// Invalid format specifier, ignore
+			}
+			} else {
+			putchar(*format);
+			count++;
 		}
-		else
-		{
-			print_buffer(buffer, &buff_ind);
-			flags = get_flags(format, &i);
-			width = get_width(format, &i, list);
-			precision = get_precision(format, &i, list);
-			size = get_size(format, &i);
-			++i;
-			printed = handle_print(format, &i, list, buffer,
-				flags, width, precision, size);
-			if (printed == -1)
-				return (-1);
-			printed_chars += printed;
-		}
+
+		format++;
 	}
 
-	print_buffer(buffer, &buff_ind);
-
-	va_end(list);
-
-	return (printed_chars);
+	va_end(args);
+	return count;
 }
 
-/**
- * print_buffer - Prints the contents of the buffer if it exist
- * @buffer: Array of chars
- * @buff_ind: Index at which to add next char, represents the length.
- */
-void print_buffer(char buffer[], int *buff_ind)
-{
-	if (*buff_ind > 0)
-		write(1, &buffer[0], *buff_ind);
-
-	*buff_ind = 0;
+int main() {
+	_printf("Hello, %s! This is a %c and a percent %% sign.\n", "world", 'c');
+	return 0;
 }

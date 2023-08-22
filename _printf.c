3c96297af6fc63dd79d469078ee1261404eaf3d0
Nhlapo <maphoskho@gmail.com>
2023-08-22 02:50:44 -0700

    printf, hopefully the correct output

--- _printf.c
+++ _printf.c
@@ -2,16 +2,18 @@
 
 int _printf(const char *format, ...) {
 
-	int count = 0;
-	char ch;
-	char *str;
-
 	va_list args;
 	va_start(args, format);
 
+	int count = 0;
+	char ch;
+	char *str;
+	unsigned int ui;
+	void *addr;
+
 	while (*format != '\0') {
 		if (*format == '%') {
-			format++;
+			format++; // Move past '%'
 
 			if (*format == 'c') {
 				ch = (char)va_arg(args, int);
@@ -24,11 +26,31 @@ int _printf(const char *format, ...) {
 					str++;
 					count++;
 				}
+			} else if (*format == 'd' || *format == 'i') {
+				int num = va_arg(args, int);
+				printf("%d", num);
+				count++;
+			} else if (*format == 'u') {
+				ui = va_arg(args, unsigned int);
+				printf("%u", ui);
+				count++;
+			} else if (*format == 'o') {
+				ui = va_arg(args, unsigned int);
+				printf("%o", ui);
+				count++;
+			} else if (*format == 'x' || *format == 'X') {
+				ui = va_arg(args, unsigned int);
+				printf("%x", ui);
+				count++;
+			} else if (*format == 'p') {
+				addr = va_arg(args, void *);
+				printf("%p", addr);
+				count++;
 			} else if (*format == '%') {
 				putchar('%');
 				count++;
-			}
-		} else {
+		}
+	} else {
 			putchar(*format);
 			count++;
 		}
